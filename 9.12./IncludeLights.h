5f80f906337fbefac45d4722af39a4cd0ad36a83
IHabro <91435877+IHabro@users.noreply.github.com>
2021-12-14 20:51:42 +0100

    Delete 9.12. directory

--- 9.12./IncludeLights.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Light.h"
-#include "PointLight.h"
-#include "DirectionalLight.h"
-#include "SpotLight.h"
--- 9.12./readme.md
+++
@@ -1,3 +0,0 @@
-# 9.12.2021
-
-5 Scenes Working, backup from Assimp attempts <br/>
